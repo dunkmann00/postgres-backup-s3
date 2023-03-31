4b21a7c33aa0a9326589c9f737318ee681b67023
George Waters <gwatersdev@gmail.com>
2023-03-31 15:43:34 -0400

    Add output describing what is running

--- run.sh
+++ run.sh
@@ -7,9 +7,12 @@ if [ "${S3_S3V4}" = "yes" ]; then
 fi
 
 if [ "${SCHEDULE}" = "**None**" ]; then
+  echo "No schedule, running backup now..."
   /usr/src/backup.sh
 else
+  echo "Found schedule, adding to crontab..."
   echo -e "${SCHEDULE} /usr/src/backup.sh > /proc/1/fd/1 2> /proc/1/fd/2\n" > /usr/src/backup-crontab
   crontab /usr/src/backup-crontab
+  echo "Starting cron..."
   exec cron -f
 fi
